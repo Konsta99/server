0b6b340e410ebd06aaf3e774d2153af50231e6ce
kstal <konradstalmach25@gmail.com>
2019-11-27 13:36:19 +0100

    XDDDDDDD

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 from typing import Optional,List, Dict
 import unittest
 #deklaracja maksymaclnej dlugosci
-#Wojciech Pełka i Konrad Stalmach 123123123
+#Wojciech Pełka i Konrad Stalmach 123123123g
 
 MAX=100
 
